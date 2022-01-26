67e34ca87775c6c9da87446d72f797991ecc6b3e
Christoph Borgmeier <cborgmeier@gmail.com>
2022-01-26 22:53:07 +0100

    Measured waveforms

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # VZemu
 VCV Rack synthesizer modules emulating aspects of an 80s electronical instrument  
+
+**Draft**: Not much to see yet.
+
+## Module Pair
+Two VCOs which may be connected via a ring modulation. One VCO may be disabled and provide a simple wave mapper for the other.
--- src/Waves.cpp
+++ src/Waves.cpp
@@ -2,6 +2,15 @@
 
 #include <cmath>
 
+float saw(float phase, float p, float u, float v)
+{
+    return phase <= p
+               ? u / p * phase
+           : phase <= v
+               ? (1 - u / p * (1 - v) - u) / (v - p) * (phase - p) + u
+               : u / p * (phase - 1) + 1;
+}
+
 Waves::Waves(size_t size)
     : wave{std::valarray<float>(size),
            std::valarray<float>(size),
@@ -12,12 +21,13 @@ Waves::Waves(size_t size)
 {
     for (size_t i = 0; i < size; ++i)
     {
-        float phase = 2 * M_PI * i / size;
-        wave[0][i] = sin(phase);
-        wave[1][i] = sin(phase) / (phase + 1) / (2 * M_PI + 1 - phase) / .34 * 5;
-        wave[2][i] = sin(phase) / (phase + .5) / (2 * M_PI + .5 - phase) / .48 * 5;
-        wave[3][i] = sin(phase) / (phase + .2) / (2 * M_PI + .2 - phase) / .71 * 5;
-        wave[4][i] = sin(phase) / (phase + .1) / (2 * M_PI + .1 - phase) /.76 * 5;
-        wave[5][i] = sin(phase) / (phase + .05) / (2 * M_PI + .05 - phase) / .8 * 5;
+        float phase = float(i) / size;
+
+        wave[0][i] = sin(2 * M_PI * phase);
+        wave[1][i] = sin(2 * M_PI * saw(phase, 0.201063, 0.298472, 0.861249));
+        wave[2][i] = sin(2 * M_PI * saw(phase, 0.122448, 0.299202, 0.915584));
+        wave[3][i] = sin(2 * M_PI * saw(phase, 0.0692947, 0.300676, 0.952308));
+        wave[4][i] = sin(2 * M_PI * saw(phase, 0.0371464, 0.300532, 0.974066));
+        wave[5][i] = sin(2 * M_PI * saw(phase, 0.0199944, 0.303999, 0.986284));
     }
 }
